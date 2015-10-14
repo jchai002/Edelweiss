a0e5f16aa6b74e581a77aaccfcf3b60c9a696ab0
Jerry Chai <jchai002@gmail.com>
2015-10-13 22:31:26 -0400

    edit logos

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,14 @@
 Admin.create(password: "nobleandwhite")
+
+Video.create({name: 'A Sound From Pennsylvania', url: 'https://www.youtube.com/embed/rx1y8ScMWp0'})
+
+Music.create(code: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=1994609732/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://edelweissofficial.bandcamp.com/album/pre-columbians">Pre-Columbians by Edelweiss</a></iframe>')
+Music.create(code: '<iframe style="border: 0; width: 350px; height: 470px;" src="https://bandcamp.com/EmbeddedPlayer/album=28858551/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/" seamless><a href="http://edelweissofficial.bandcamp.com/album/fiasco">Fiasco by Edelweiss</a></iframe>')
+
+Picture.create(link:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-prn2/t31.0-8/10923823_947425511936626_262234132565191213_o.jpg")
+Picture.create(link:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xap1/t31.0-8/241229_225826817429836_6096707_o.jpg")
+Picture.create(link:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10624737_887107934635051_9208350287725010612_n.jpg?oh=45679b04e1ea1310a27f47cddbed667c&oe=568A7CDF")
+Picture.create(link:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-frc3/t31.0-8/901147_651744514838062_103166658_o.jpg")
+Picture.create(link:"https://scontent-lga3-1.xx.fbcdn.net/hphotos-ash2/v/t1.0-9/1932473_909899999022511_810881487208187947_n.jpg?oh=7b3beba3818627a73b7639ea50f3a212&oe=56CB191E")
+
+
